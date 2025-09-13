3c4911211bea1a44fdd721e0f4c32ef52dce7e08
Miguel Ángel López Carot <malopez@iti.es>
2025-09-13 02:31:58 +0200

    feat: renombrar la extensión a "PAco" y agregar definición de tipos para tareas en el popup

--- src/popup.ts
+++ src/popup.ts
@@ -1,5 +1,22 @@
 import { ListStorage } from "./background";
-import { Task, TaskPriority } from "./types";
+export enum TaskPriority {
+  VERY_HIGH = "VERY_HIGH",
+  HIGH = "HIGH",
+  MEDIUM = "MEDIUM",
+  LOW = "LOW",
+  VERY_LOW = "VERY_LOW",
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  done: boolean;
+  description?: string;
+  createdAt: string;
+  updatedAt: string;
+  dueDate: string;
+  priority: TaskPriority;
+}
 
 class TaskManager {
   private storage: ListStorage<Task>;
